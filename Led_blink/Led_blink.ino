ff92d084ae0a85be8538232c542b5b2bc043912b
veronicavvf <veronicavargas@usp.br>
2026-05-19 16:47:58 -0300

    add LED blink and webserver examples

---
+++ Led_blink/Led_blink.ino
@@ -0,0 +1,37 @@
+const int led1 = 1;
+const int led2 = 2;
+const int led3 = 3;
+const int led4 = 4;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led1, HIGH);
+  delay(1000);
+  digitalWrite(led1, LOW);
+  delay(1000);
+
+  digitalWrite(led2, HIGH);
+  delay(1000);
+  digitalWrite(led2, LOW);
+  delay(1000);
+
+  digitalWrite(led3, HIGH);
+  delay(1000);
+  digitalWrite(led3, LOW);
+  delay(1000);
+
+  digitalWrite(led4, HIGH);
+  delay(1000);
+  digitalWrite(led4, LOW);
+  delay(1000);
+
+}
